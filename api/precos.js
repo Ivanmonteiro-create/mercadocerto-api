353c2246117c1bc972dd9be929568d32c968dc6a
Dorgivan Monteiro <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-31 15:39:28 +0000

    Update precos.js

--- api/precos.js
+++ api/precos.js
@@ -2,22 +2,18 @@
 
 export default function handler(req, res) {
   const dados = [
-    // arroz
     { nome: "Arroz Agulha 1kg", mercado: "Minipreço", preco: 0.9 },
     { nome: "Arroz Agulha 1kg", mercado: "Pingo Doce", preco: 0.99 },
     { nome: "Arroz Agulha 1kg", mercado: "Continente", preco: 1.05 },
 
-    // leite
     { nome: "Leite Meio Gordo 1L", mercado: "Continente", preco: 0.89 },
     { nome: "Leite Meio Gordo 1L", mercado: "Pingo Doce", preco: 0.92 },
     { nome: "Leite Meio Gordo 1L", mercado: "Minipreço", preco: 0.95 },
 
-    // massa
     { nome: "Massa Esparguete 500g", mercado: "Continente", preco: 0.69 },
     { nome: "Massa Esparguete 500g", mercado: "Pingo Doce", preco: 0.72 },
     { nome: "Massa Esparguete 500g", mercado: "Minipreço", preco: 0.75 },
 
-    // óleo
     { nome: "Óleo de Girassol 1L", mercado: "Pingo Doce", preco: 1.75 },
     { nome: "Óleo de Girassol 1L", mercado: "Continente", preco: 1.79 },
     { nome: "Óleo de Girassol 1L", mercado: "Minipreço", preco: 1.83 }
