0ba99adf2f7bed95f071ae2d6c8ca46b80ed92a5
Dorgivan Monteiro <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-31 14:53:24 +0000

    Update precos.js

--- api/precos.js
+++ api/precos.js
@@ -1,11 +1,5 @@
-// api/precos.js
-
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  // podes depois trocar isto para vir de uma base de dados
-  const dados = [
-    // ARROZ
+export default function handler(req, res) {
+  const produtos = [
     {
       nome: "Arroz Agulha 1kg",
       mercado: "Minipreço",
@@ -19,76 +13,14 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
     {
       nome: "Arroz Agulha 1kg",
       mercado: "Continente",
-      preco: 1.0
+      preco: 1.05
     },
-
-    // FEIJÃO
-    {
-      nome: "Feijão Carioca 1kg",
-      mercado: "Minipreço",
-      preco: 1.29
-    },
-    {
-      nome: "Feijão Carioca 1kg",
-      mercado: "Pingo Doce",
-      preco: 1.35
-    },
-    {
-      nome: "Feijão Carioca 1kg",
-      mercado: "Continente",
-      preco: 1.42
-    },
-
-    // LEITE
     {
       nome: "Leite Meio Gordo 1L",
       mercado: "Continente",
       preco: 0.89
-    },
-    {
-      nome: "Leite Meio Gordo 1L",
-      mercado: "Pingo Doce",
-      preco: 0.95
-    },
-
-    // MASSA
-    {
-      nome: "Massa Esparguete 500g",
-      mercado: "Continente",
-      preco: 0.69
-    },
-    {
-      nome: "Massa Esparguete 500g",
-      mercado: "Minipreço",
-      preco: 0.7
-    },
-    {
-      nome: "Massa Esparguete 500g",
-      mercado: "Pingo Doce",
-      preco: 0.72
-    },
-
-    // ÓLEO
-    {
-      nome: "Óleo de Girassol 1L",
-      mercado: "Pingo Doce",
-      preco: 1.75
-    },
-    {
-      nome: "Óleo de Girassol 1L",
-      mercado: "Continente",
-      preco: 1.79
-    },
-    {
-      nome: "Óleo de Girassol 1L",
-      mercado: "Minipreço",
-      preco: 1.83
     }
   ];
 
-  // permitir o Expo/telemóvel pedir isto
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-
-  return res.status(200).json(dados);
+  res.status(200).json(produtos);
 }
